b4db45de5b9ba788e443b8e2e01b1c88c6778bc0
sabrinasukhin <sabrina@sukhin.com>
2017-07-02 22:04:21 -0400

    Broken rail scripts

---
+++ RailMover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailMover : MonoBehaviour
+{
+    public Rail rail;
+    public Transform lookAt;
+
+    private Transform thisTransform;
+
+    private void Start()
+    {
+        thisTransform = transform;
+    }
+}
