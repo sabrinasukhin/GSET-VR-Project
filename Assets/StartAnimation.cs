774ceab0ea4f02b2593dac9af973972b63f8e0d1
mjrnguyen <mjrnguyen@gmail.com>
2017-07-13 20:35:53 -0400

    Fixed Animation

---
+++ Assets/StartAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class StartAnimation : MonoBehaviour {
+	//public Animation StartAnim;
+
+	Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		animator.Play ("TestAnim");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
