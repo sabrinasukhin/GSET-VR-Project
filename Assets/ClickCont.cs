e6eedf4c6a0a89b60e5039544c327b5a0c62ae7c
mjrnguyen <mjrnguyen@gmail.com>
2017-07-15 15:19:21 -0400

    Changed ClickCont

--- Assets/ClickCont.cs
+++ Assets/ClickCont.cs
@@ -2,7 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public void ClickCont()
-{
-	
+public class VariablesAndFunctions : MonoBehaviour
+{   
+	int myInt = 5;
+
+
+	void Start ()
+	{
+		myInt = MultiplyByTwo(myInt);
+		Debug.Log (myInt);
+	}
+
+
+	int MultiplyByTwo (int number)
+	{
+		int ret;
+		ret = number * 2;
+		return ret;
+	}
 }
+
