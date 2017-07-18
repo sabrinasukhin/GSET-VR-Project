fc4d1c34c0f33debd11f5f779850ff4b4bea746b
mjrnguyen <mjrnguyen@gmail.com>
2017-07-18 15:20:50 -0400

    fixed animation

---
+++ Assets/Scripts/LogTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void PrintFloat (float theValue) {
+		Debug.Log ("PrintFloat is called with a value of " + theValue);
+	}
+}
